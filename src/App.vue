ec465af6a5e5f3c6977ce2d4dc937ae977a84186
Sayuti Daniel <sayutidaniel@gmail.com>
2021-02-11 15:55:49 +0800

    multiple backgrounds bug

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="logo"></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style lang="scss">
+.logo {
+  width: 200px;
+  height: 200px;
+  background: url("./assets/logo.png") top left / 100px no-repeat,
+    url("./assets/logo.png") bottom right / 100px no-repeat;
+}
+</style>
